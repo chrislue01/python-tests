93fd0b2e24677ec98ac2dc82b129e324de101852
Christopher Lue <99819387+chrislue01@users.noreply.github.com>
2022-03-07 20:50:31 -0500

    Add files via upload

---
+++ grocery_list.py
@@ -0,0 +1,12 @@
+grocery_list = ["milk","bread","eggs","peanut butter","jelly"]
+print(grocery_list)
+
+#change peanut butter to Almond Butter
+grocery_list[3] = "Almond_Butter"
+print(grocery_list)
+
+grocery_list.remove("jelly")
+print(grocery_list)
+
+grocery_list.append("coffee")
+print(grocery_list)
